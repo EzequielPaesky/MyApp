ee0652dd02dc8e2569759ceaf4b970cfa5e64bf1
EzequielPaesky <89224799+EzequielPaesky@users.noreply.github.com>
2022-01-24 22:03:43 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import NavBar from './components/NavBar';
+
+import './App.css';
+
+const App = () =>{
+  return (
+    <>
+    <NavBar />
+      <h1>Mundial Qatar 2022</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+const NavBar = () =>{
+
+   return (
+        <nav class="nav">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Adultos</a></li>
+                <li><a href="">Niños</a></li>
+                <li><a href="">Merchandesing</a></li>
+            </ul>
+            </nav>
+   );
+}
+
+export default NavBar; 
